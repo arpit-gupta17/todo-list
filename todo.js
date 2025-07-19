2900a92cbc0c99d4ce60242b9cc83e4da6e77f43
arpit-gupta17 <arpitgupta170577@gmail.com>
2025-07-19 16:08:38 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,30 @@
+let todo=[]; 
+
+
+let req=prompt("please enter your request");
+
+
+while (true){
+    if(req == "quit") {
+        console.log("quitting the app");
+        break;
+    }
+     
+    if(req == "list"){
+        console.log("----------------------");
+        for(let i=0; i<todo.length; i++){
+          console.log(i,todo[i]);
+        }
+           console.log("---------------------");
+    }else if(req=="add"){
+        let task = prompt("add your task");
+        todo.push(task);
+        console.log("task is added successfully");
+
+    }else if(req == "delete"){
+        let idx = prompt("please enter the index you want to delete");\
+        todo.splice(idx,1);
+        console.log("task deleted")
+    }
+    req = prompt("please enter your request"); 
+}
